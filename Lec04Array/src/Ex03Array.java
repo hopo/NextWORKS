2bdc38a5a090bdd958bf60f3b99010da27feab94
hopo <hpsight@gmail.com>
2018-04-16 16:52:24 +0900

    array

--- Lec04Array/src/Ex03Array.java
+++ Lec04Array/src/Ex03Array.java
@@ -15,19 +15,21 @@ public class Ex03Array {
 		System.out.println();
 		// lotto = { 1, 2, 3, 4, 5, ... 43, 44, 45 }, 순차적인 값들이 배열 안으로 들어감.
 		
+		// shuffle
 		// for 문을 돌려 임의의 위치의 값과 swap
-		int ri, temp;
+		int idx, temp;
 		for(int i = 0; i < 100; i++) {
-			ri = (int)(Math.random() * 45); 
+			idx = (int)(Math.random() * lotto.length); 
+
 			temp = lotto[0];
-			lotto[0] = lotto[ri];
-			lotto[ri] = temp;
+			lotto[0] = lotto[idx];
+			lotto[idx] = temp;
 		}
 
 		// print lotto[0] ~ lotto[5]
-		System.out.print("Six number: ");
+		System.out.println("================ 금주의 로또 당첨번호 ================");
 		for (int i = 0; i < 6; i++) {
-			System.out.print(lotto[i] + ", ");
+			System.out.print(lotto[i] + " ");
 		}
 		System.out.println();
 
