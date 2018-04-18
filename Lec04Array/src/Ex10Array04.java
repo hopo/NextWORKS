cdeefa6513520d444d683e30ffa235ab64523180
hopo <hpsight@gmail.com>
2018-04-18 14:51:22 +0900

    0

--- Lec04Array/src/Ex10Array04.java
+++ Lec04Array/src/Ex10Array04.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Ex10MembersInfo04 {
+public class Ex10Array04 {
 
 	public static void main(String[] args) throws IOException {
 		// EXAM)
---
+++ Lec04Array/src/Ex10Array05.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// sensei solution
+public class Ex10Array05 {
+
+	public static void main(String[] args) throws IOException {
+		// EXAM)
+		// "이름\t 나이\t 성별\t 전화\t 주소\n"		
+		// 3명의 정보를 받아라
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
