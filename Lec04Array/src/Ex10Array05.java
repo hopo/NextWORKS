37cb5e05d21eea4015b8362c53802d7e2f4aad5a
hopo <hpsight@gmail.com>
2018-04-18 15:11:38 +0900

    -

--- Lec04Array/src/Ex10Array05.java
+++ Lec04Array/src/Ex10Array05.java
@@ -11,6 +11,39 @@ public class Ex10Array05 {
 		// 3명의 정보를 받아라
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int manCnt = 3;
+
+		String[] name = new String[manCnt];
+		int[] age = new int[manCnt];
+		char[] gender = new char[manCnt];
+		String[] phone = new String[manCnt];
+		String[] address = new String[manCnt];
+	
+		for(int i = 0; i < manCnt; i++) {
+			System.out.print("이름 : ");
+			name[i] = reader.readLine();
+
+			System.out.print("나이 : ");
+			age[i] = Integer.parseInt(reader.readLine());
+
+			System.out.print("성별(M/F) : ");
+			gender[i] = (char)reader.read();
+			reader.readLine();	// '\n' 제거
+
+			System.out.print("전화 : ");
+			phone[i] = reader.readLine();
+
+			System.out.print("주소 : ");
+			address[i] = reader.readLine();
+		}
+
+		System.out.println(">>>>>>>>>>>>>>> 회원주소록 <<<<<<<<<<<<<<<");
+		System.out.println("이름\t 나이\t 성별\t 전화\t 주소");
+		
+		for(int i = 0; i < manCnt; i++) {
+			System.out.printf("%s\t %d\t %c\t %s\t %s\n",
+					name[i], age[i], gender[i], phone[i], address[i]);
+		}
 	}
 
 }
