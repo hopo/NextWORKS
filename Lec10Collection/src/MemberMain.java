0990a82e0df2d9dae75ccb8f26b2fe9a81364d69
hopo <hpsight@gmail.com>
2018-05-01 20:07:52 +0900

    -

--- Lec10Collection/src/MemberMain.java
+++ Lec10Collection/src/MemberMain.java
@@ -8,40 +8,43 @@ public class MemberMain {
 
 	public static void main(String[] args) throws IOException {
 
-		// 입력 받을 준비
+		// !!! 입력 받을 준비
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 		List<Member> members = new ArrayList<Member>();
-		int num = 2;
+		Member member = null;
+		char term;
 
-		for (int i = 0; i < num; i++) {
-			Member member = new Member();
-			members.add(member);
+		do {
+			member = new Member();
 
-			System.out.print("이름 : ");
+			System.out.print("이름: ");
 			member.setName(reader.readLine());
 
-			System.out.print("나이 : ");
+			System.out.print("나이: ");
 			member.setAge(Integer.parseInt(reader.readLine()));
 
-			System.out.print("성별(m/f) : ");
+			System.out.print("성별(m/f): ");
 			member.setGender((char) reader.read());
 			reader.readLine();
 
-			System.out.print("전화 : ");
+			System.out.print("전화: ");
 			member.setPhone(reader.readLine());
-		}
-		
-		// 출력이요
-		System.out.println("========== 회원목록 ==========");
+
+			members.add(member);
+
+			System.out.print(">> 더 추가하시겠습니까(y/n)? ");
+			term = (char) reader.read();
+			reader.readLine();
+
+		} while (term == 'y');
+
+		// !!! 출력이요
+		System.out.println("============= 회원목록 =============");
 		System.out.println("이름\t 나이\t 성별\t 전화번호");
-		for(int i = 0; i < members.size(); i++) {
-			System.out.printf(
-				"%s\t %d\t %c\t %s\n",
-				members.get(i).getName(), members.get(i).getAge(), members.get(i).getGender(), members.get(i).getPhone()
-			);
+		for (Member m : members) {
+			System.out.printf("%s\t %d\t %c\t %s\n", m.getName(), m.getAge(), m.getGender(), m.getPhone());
 		}
-		System.out.println("==============================");
+		System.out.println("==================================");
 
 	}
 }
