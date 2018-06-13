d68923cc4a176cafb8a37116edeb43ed16c6102e
hopo <hpsight@gmail.com>
2018-06-13 12:09:58 +0900

    Lec99

--- lecture-JAVA/SourceCode/Lec99Project/src/pc33/gdrive14/GdriveCommand.java
+++ lecture-JAVA/SourceCode/Lec99Project/src/pc33/gdrive14/GdriveCommand.java
@@ -1,4 +1,4 @@
-package pc33.gdrive;
+package pc33.gdrive14;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ lecture-JAVA/SourceCode/Lec99Project/src/pc33/gdrive14/Gpath.java
@@ -0,0 +1,35 @@
+package pc33.gdrive14;
+
+import java.io.File;
+
+public class Gpath {
+
+	private static String homePath = System.getenv("HOME");
+	private static String gdrivePath = homePath + "/.gdrive";	// ;디렉토리.
+	private static String gdrive = gdrivePath + "/gdrive";		// ;파일.
+	private static String downloadPath = homePath + "/Desktop";
+
+	public static String getHomePath() {
+		return homePath;
+	}
+
+	public static String getGdrivePath() {
+		if (!new File(gdrivePath).exists()) {
+			return null;
+		}
+		return gdrivePath;
+	}
+
+	public static String getGdrive() {
+
+		if (!new File(gdrive).exists()) {
+			return null;
+		}
+		return gdrive;
+	}
+	
+	public static String getDownloadPath() {
+		return downloadPath;
+	}
+
+}
