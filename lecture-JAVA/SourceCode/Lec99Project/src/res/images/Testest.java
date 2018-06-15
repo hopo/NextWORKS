b9ecbc37003dfe5fe728db049a47cdfddc4f0470
hopo <hpsight@gmail.com>
2018-06-15 12:06:38 +0900

    0615 JS

---
+++ lecture-JAVA/SourceCode/Lec99Project/src/res/images/Testest.java
@@ -0,0 +1,25 @@
+package res.images;
+
+public class Testest {
+	public static void main(String[] args) {
+
+		char varChar = 0;
+		short varShort = 0;
+		int varInt = 0;
+		long varLong = 0;
+		byte varByte = 0;
+		boolean varBoolean = false;
+		float varFloat = 0;
+		double varDouble = 0;
+
+		System.out.println(varChar);
+		System.out.println(varShort);
+		System.out.println(varInt);
+		System.out.println(varLong);
+		System.out.println(varByte);
+		System.out.println(varBoolean);
+		System.out.println(varFloat);
+		System.out.println(varDouble);
+	}
+
+}
