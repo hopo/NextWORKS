324ce64ca8732b941830c8a162074b44c656941d
hopo <hpsight@gmail.com>
2018-06-19 17:53:03 +0900

    0619 Front

--- lecture-Front/jsstudy/Week05_June18/homework0618.js
+++ lecture-Front/jsstudy/Week05_June18/homework0618.js
@@ -1,25 +1,18 @@
 "use strict"
 
-
 var rNum = 3;
 var cNum = 3;
 
-var nodeTable = null;
-var nodeTbody = null;
-var nodeTr = null;
-var nodeTd = null;
-var nodeInput = null;
-
-nodeTable = document.createElement("table");
-nodeTbody = document.createElement("tbody");
+var nodeTable = document.createElement("table");
+var nodeTbody = document.createElement("tbody");
 
 var n = 1;
 for (let i = 0; i < rNum; i++) {
-    nodeTr = document.createElement("tr");
+    var nodeTr = document.createElement("tr");
 
     for (let j = 0; j < cNum; j++) {
-        nodeTd = document.createElement("td");
-        nodeInput = document.createElement("input");
+        var nodeTd = document.createElement("td");
+        var nodeInput = document.createElement("input");
 
         nodeInput.setAttribute("type", "button");
         nodeInput.setAttribute("value", `${n++}`);
