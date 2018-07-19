6ae35f13296ef5d6d17da6dee681d0370619279c
hopo <hpsight@gmail.com>
2018-07-19 23:17:52 +0900

    0719 Front

---
+++ lecture-Front/jsstudy/Nexttutorials/board/data/boardItems.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+var titles = `Show me the money,안녕하세요. 오랔ㅋㅋ클입니다.,Function is Jung Yo!,
+이것은 가짜 데이터입니다만...,자바는 껌이다!,어머님은 짜장면이 싫다고 하셨지,짬뽕이 좋아,
+にほんごがはなせません,냐옹냐옹 멍멍,호우~`;
+
+var names = '오고쉬,오대수,오바마,오까네,오디오,오레오,오징오,오덕후,오케이,오그만';
+
+var ttt = titles.split(',');
+var nnn = names.split(',');
+
+
+var datas = [];
+
+datas[98] = `Oh gosh 난리야 Oh gosh
+맞아 난 좀 기분파
+헤 금방 또 사랑에 빠져
+Yeah yeah yeah yeah
+새것만 좋아해요 반짝거리죠
+다들 그렇잖아요 맞죠
+
+Peek A Boo 설렐 때만 사랑이니까
+La la la la la
+내 친구 모두 소리쳐
+넌 정말 문제야
+I’m fine fine fine fine fine fine
+
+Peek Peek A Peek A Boo
+Peek Peek A Peek A Boo
+흥이 난 여우 그런 나라구
+Peek Peek A Peek A Boo
+
+I said 1 2 3
+Play the game again
+버튼을 눌러보자 쿵푸만큼 빨리
+중간에 내 맘 변해도 놀라지 말기
+혹시 끌리지 않니 그럼 Excuse me
+자 전화해요 밤새 또 놀러 가요 함께
+Restart a game
+돌진해 롤링해 블랑카'`;
