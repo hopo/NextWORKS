a9f9e313384a5af21bd47dc75c2f8ed5b9ef1604
hopo <hpsight@gmail.com>
2018-07-30 17:52:53 +0900

    .;

--- testBS4/main.py
+++ testBS4/main.py
@@ -1,26 +1,25 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import requests as req
+import requests
+import lxml
+import time
 
 
 url = 'https://datalab.naver.com/keyword/realtimeList.naver?datetime=2018-07-30T09:00:00'
 
+'''
 driver = webdriver.Firefox(executable_path='./geckodriver')
 
 driver.get(url)
 
 html = driver.page_source
-
-soup = BeautifulSoup(html)
-
-print(soup.prettify())
-
-
 '''
-html = req.get(url)
+
+html = requests.get(url, "lxml")
+
+time.sleep(10)
 
 soup = BeautifulSoup(html.text)
 
 print(soup.prettify())
-'''
 
