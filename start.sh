312475cb09656ed30a4efc60f03487937637e68d
hopo <hpsight@gmail.com>
2018-09-06 08:59:15 +0900

    ,

---
+++ start.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# ctrl <-> caps
+setxkbmap -option "ctrl:swapcaps"
+
+# esc <-> tilde
+#xmodmap -e "keycode 9 = grave asciitilde"
+#xmodmap -e "keycode 49 = Escape"
+
+
+# redshift set
+redshift -O 4200
